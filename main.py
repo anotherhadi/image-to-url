ab8ecfd260b067d3b5d5f464cebe14541a4e9510
Hadi <112569860+0x68616469@users.noreply.github.com>
2023-02-18 23:50:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ app = Flask(__name__, template_folder='templateFiles', static_folder='staticFile
 info_path = r"./settingFiles/info.json"
 settings_path = r"./settingFiles/settings.json"
 
+# Load Settings
 with open(settings_path, "r+") as f:
     settings = json.load(f)
 
+# Load informations about uploaded images
 with open(info_path, "r+") as f:
     info = json.load(f)
 
+# Check if the "src" folder exist (Used to store images)
+if not os.path.isdir("./src"):
+    os.mkdir("./src")
+
+# Return a random string of length length (Used to generate image name)
 def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
+# Check if the file is in the allowed extensions list
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in settings["ALLOWED_EXTENSIONS"]
 
@@ -25,7 +33,14 @@ def allowed_file(filename):
 def index(): return redirect(url_for("new"))
 
 @app.route('/<string:path>', methods=['GET'])
-def image(path): return send_from_directory(app.config['UPLOAD_FOLDER'], path)
+def image(path):
+    path = secure_filename(path)
+    if not os.path.isfile(app.config['UPLOAD_FOLDER']+ path):
+        folder = "staticFiles/"
+        path = "404.png"
+        return send_from_directory(folder, path)
+
+    return send_from_directory(app.config['UPLOAD_FOLDER'], path)
 
 @app.route('/explore', methods=['GET'])
 def explore_home(): return redirect(url_for("explore", level=0))
@@ -33,8 +48,13 @@ def explore_home(): return redirect(url_for("explore", level=0))
 @app.route('/explore/<int:level>', methods=['GET'])
 def explore(level=0):
     if settings["EXPLORE"] == True:
-        images = list(info.items())[level:level+9]
-        return images
+        images = list(info.items())[level*9:level*9+9]
+        context={
+            "images":images,
+            "level":level,
+            "url":settings["URL"],
+        }
+        return render_template('explore.html', **context)
     else:
         flash("Explore desactivated")
         return redirect(url_for("new"))
@@ -45,7 +65,7 @@ def new():
     if not hash_nsec in settings["NSEC"]:
         if settings["ALLOW_EVERYONE"] == False:
             flash(hash_nsec + " : Not authorized to post")
-            return redirect(request.url)
+            return redirect(url_for("login"))
 
     if request.method == "POST":
         if "file" not in request.files:
@@ -97,7 +117,7 @@ def login():
     if request.method == "POST":
         nsec = request.form["nsec"]
         nsec_hash = hashlib.md5(str(nsec).encode("utf-8"))
-        resp = make_response(render_template("login.html"))
+        resp = make_response(redirect(url_for("new")))
         resp.set_cookie('nsec', nsec_hash.hexdigest())
         flash("Hash of nsec successfully written to 'nsec' cookie")
         return resp
